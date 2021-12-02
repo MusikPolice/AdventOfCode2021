bd2a3da13f8e3c03aeae4d8012d02f6734ee7e96
MusikPolice <jonfritz@gmail.com>
2021-12-01 21:05:01 -0500

    Day 1 Part 2

--- src/main/kotlin/ca/jonathanfritz/adventofcode2021/day1/SonarSweep.kt
+++ src/main/kotlin/ca/jonathanfritz/adventofcode2021/day1/SonarSweep.kt
@@ -13,11 +13,24 @@ class SonarSweep {
         }
         println("Part 1: $count measurements are larger than the previous measurement")
     }
+
+    fun part2(depths: List<Int>) {
+        var count = 0;
+        depths.forEachIndexed { i, _ ->
+            if (i+3 >= depths.size) return@forEachIndexed
+            val sw1 = depths[i] + depths[i+1] + depths[i+2]
+            val sw2 = depths[i+1] + depths[i+2] + depths[i+3]
+            if (sw2 > sw1) {
+                count++
+            }
+        }
+        println("Part 2: $count sums are larger than the previous sum")
+    }
 }
 
 fun main() {
     val sonarSweep = SonarSweep()
-    sonarSweep.part1(
-        Utils.loadFromFile("day1.txt") { it.toInt() }
-    )
+    val depths = Utils.loadFromFile("day1.txt") { it.toInt() }
+    sonarSweep.part1(depths)
+    sonarSweep.part2(depths)
 }
